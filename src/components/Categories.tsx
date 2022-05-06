57860a85a091b94538780bb2d6aaaaf687e5b278
Alok Mishra <alokmish.pro@gmail.com>
2022-05-06 06:24:20 +0000

    Initial commit

---
+++ src/components/Categories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { IMenuItem } from "../data";
+
+interface CategoriesProps {
+  categories: string[];
+  filterItems: IMenuItem[];
+}
+
+const Categories: React.FC<CategoriesProps> = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
